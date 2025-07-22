fe4d0970ad849384202a79a7919296f8039c1b19
Nodirbek12345 <natgress000@gmail.com>
2025-07-22 10:00:26 +0500

    add products

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,23 @@
-import Hero from "../../components/hero/Hero"
-import HomeType from "../../components/home-type/HomeType"
-import Products from "../../components/products/Products"
+import React, { useState } from "react";
+import Hero from "../../components/hero/Hero";
+import HomeType from "../../components/home-type/HomeType";
+import Products from "../../components/products/Products";
+import { useFetch } from "../../hooks/useFetch";
+import Skeleton from "../../components/products/skeleton.jsx";
 
 const Home = () => {
+  const { data, error, loading } = useFetch("/products", {
+    limit: 8,
+    skip: 0,
+  });
   return (
     <div>
-      <Hero/>
-      <HomeType/>
-      <Products/>
+      <Hero />
+      <HomeType />
+      {error && <p>something went wrong</p>}
+      {loading ? <Skeleton /> : <Products data={data} />}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
