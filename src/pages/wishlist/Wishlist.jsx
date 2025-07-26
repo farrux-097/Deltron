1800b6323b62289147bc3ecea57d9bf44eeebeba
farrux-097 <olimovfarrux907@gmail.com>
2025-07-26 11:35:59 +0500

    wishlist qism va productga uzgartsh kiritildi

--- src/pages/wishlist/Wishlist.jsx
+++ src/pages/wishlist/Wishlist.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import Products from "../../components/products/Products";
+import { useStore } from "../../zustand/useStore";
 
 const Wishlist = () => {
+  const { wishlist } = useStore();
   return (
-    <div>
-        <h2>Hello</h2>
+    <div className="container mb-[20px]">
+      <Products data={{ products: wishlist }} showMoreButton={false} title="Your Wishlist" />
     </div>
   )
-}
+};
 
-export default Wishlist
+export default Wishlist;
+
